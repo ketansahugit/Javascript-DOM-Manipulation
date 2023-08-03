5ee1517ab1189c46690bcb4d7ffad45849a51f94
Ketan Sahu <rorozoro9888@gmail.com>
2023-08-03 21:30:52 +0530

    added js for accordion

--- Project-3/script.js
+++ Project-3/script.js
@@ -1,10 +1,12 @@
 // Variable 
 
-// const accordion = document.getElementsByClassName('content-container');
+const accordion = document.getElementsByClassName('content-container');
 
-// for( i = 0; i < accordion.length; i++) {
+ for( i = 0; i < accordion.length; i++) {
 
-accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active');
-})
+    accordion[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+    });
+
+}
 
